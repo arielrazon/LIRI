9568a94a19cd82e06ff09bb774058408ef8f547c
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-08-21 16:20:24 -0400

    added spotify api keys and added homework instructions to gitignore

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: process.env.c2762e741cf64e56be7da27f6562f7b1,
+    secret: process.env.c8e0cf89245348a298f5ba45830f48e8
 };
