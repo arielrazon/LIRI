befcb10ec397735d3117671f9b5b08c2238b5973
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-08-25 12:36:32 -0400

    added spotify functionality

--- liri.js
+++ liri.js
@@ -41,10 +41,20 @@ else if (userCommand == "spotify-this-song") {
         query: searchTerm
     },
         function (err, data) {
+            var items = data.tracks.items
+            console.log(items[0])
+            var songName = items[0].name
+            var artist = items[0].artists[0].name
+            var album = items[0].album.name
+            var previewLink = items[0].preview_url
+            console.log(artist)
             if (err) {
                 return console.log('Error occured: ' + err);
             }
-            else { console.log(data) }
+            else if (songName) {
+                console.log("Song Info" + "\n" + "____________________" + "\n\n" + "The Artist of this Song is: " + artist + "\n\n" + "The Song's name is: " + songName + "\n\n" + "This is the album that the song is from: " + album + "\n\n" + "Here is a preview link of the song: \n" + previewLink + "\n")
+
+            }
         });
 }
 else if (userCommand == "movie-this") {
