a91232322733d1ad7d0f54f324a9ee159c48f882
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-08-25 16:39:00 -0400

    added log file and updating do-what-it-says command

--- liri.js
+++ liri.js
@@ -1,27 +1,33 @@
-//current to-do:
-//get spotify api integration to work
-//get omdb integration to work
-//do-what-it-says
-//create a nice readme.md file
 
+//adding dotenv for api keys
 require("dotenv").config();
+//adding moment api from NPM
 var moment = require("moment");
+//adding spotify api keys
 var keys = require("./keys.js");
+//adding NPM spotify api
 var Spotify = require('node-spotify-api')
 var spotify = new Spotify(keys.spotify);
-
+//adding FS
 var fs = require("fs")
+//the third thing written in CLI is the user's command
 var userCommand = process.argv[2]
+//everything after the third thing is the search term
 var searchTerm = process.argv.splice(3).join("+")
+//adding axios
 var axios = require("axios")
-
+//log command for testing
 if (userCommand == "log") {
     console.log(searchTerm)
 }
+//looking up concerts from bandsintown api
 else if (userCommand == "concert-this") {
+    //axios pull from bandsintown api with user's searchTerm
     axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
         .then(function (response) {
+            //cleaner way to pull JSON response
             var data = response.data
+            //if the array has information in it, do the following:
             if (data[0]) {
                 console.log("Your band has the following upcoming events:")
                 for (i = 0; i < data.length; i++) {
@@ -53,7 +59,7 @@ else if (userCommand == "spotify-this-song") {
                 if (data.tracks.total == 0) {
                     //console log Ace of Base message
                     console.log("Unfortunately your song isn't listed! So we gave you Ace of Bass" + "\n" + "____________________" + "\n\n")
-                    //then do a new search
+                    //then do a new search for ace of bass
                     spotify.search({
                         type: 'track',
                         query: 'the sign ace of base'
@@ -64,7 +70,7 @@ else if (userCommand == "spotify-this-song") {
                             if (err) {
                                 return console.log('Error occured: ' + err);
                             }
-                            //if new data, play ace of bass - with vars
+                            //if new data, play ace of bass
                             if (data) {
 
                                 var items = data.tracks.items
@@ -75,14 +81,15 @@ else if (userCommand == "spotify-this-song") {
                                 var previewLink = items[0].preview_url
 
 
-                                console.log("The Song's name is: " + songName + "\n\n" + "The album that the song is from is: " + album + "\n\n" + "Here is a preview link of the song: \n" + previewLink + "\n")
+                                console.log("The Artist of this Song is: " + artist + "\n\n" + "The Song's name is: " + songName + "\n\n" + "The album that the song is from is: " + album + "\n\n" + "Here is a preview link of the song: \n" + previewLink + "\n")
                             }
                         })
 
 
                 }
+                //if there are results
                 else if (data.tracks.total > 0) {
-
+                    //display the following
                     var items = data.tracks.items
 
                     var songName = items[0].name
@@ -96,38 +103,52 @@ else if (userCommand == "spotify-this-song") {
             }
         });
 }
+//if user command is movie-this:
 else if (userCommand == "movie-this") {
-
+    //if the user inputs a searchterm:
     if (searchTerm) {
-
+        //do an axios pull
         axios({
             method: 'get',
             url: 'http://www.omdbapi.com/?apikey=trilogy&t=' + searchTerm
         }).then(function (response) {
+            // if it works, display the following:
             var data = response.data;
-            //country, language, plot, actors
-            var title = data.Title;
-            var year = data.Year;
-            var plot = data.Plot;
-            var actors = data.Actors;
-            var language = data.Language;
-            var country = data.Country;
-            var ratings = data.Ratings
-            console.log("Movie Info" + "\n" + "____________________" + "\n\n" + "The Title of this Film is: \n" + title + "\n\n" + "The Film was released in the year: \n" + year + "\n\n" + "The Film was rated as follows:\n")
-            for (i = 0; i < ratings.length; i++) {
-                console.log("   " + ratings[i].Source + " rated it " + ratings[i].Value + "\n")
+            if (!data.Error) {
+                console.log(data)
+                //movie title
+                var title = data.Title;
+                //movie release year
+                var year = data.Year;
+                //movie plot
+                var plot = data.Plot;
+                //movie actors
+                var actors = data.Actors;
+                //movie language
+                var language = data.Language;
+                //release country
+                var country = data.Country;
+                //movie ratings
+                var ratings = data.Ratings
+                console.log("Movie Info" + "\n" + "____________________" + "\n\n" + "The Title of this Film is: \n" + title + "\n\n" + "The Film was released in the year: \n" + year + "\n\n" + "The Film was rated as follows:\n")
+                //put a for loop for all ratings inside of the CLI log:
+                for (i = 0; i < ratings.length; i++) {
+                    console.log("   " + ratings[i].Source + " rated it " + ratings[i].Value + "\n")
+                }
+                //continue displaying the information:
+                console.log("The Film was produced in: \n" + country + "\n\n" + "The Film is available in the following languages: \n" + language + "\n\n" + "The Film had the following actors: \n" + actors + "\n\n" + "Plot summary: \n" + plot)
             }
-            console.log("The Film was produced in: \n" + country + "\n\n" + "The Film is available in the following languages: \n" + language + "\n\n" + "The Film had the following actors: \n" + actors + "\n\n" + "Plot summary: \n" + plot)
-
+            else { console.log("I'm sorry but your search returned nothing!") }
         })
     }
+    //if user has no input at all, then
     else {
         axios({
             method: 'get',
+            //display mr nobody
             url: 'http://www.omdbapi.com/?apikey=trilogy&t=Mr+Nobody'
         }).then(function (response) {
             var data = response.data;
-            //country, language, plot, actors
             var title = data.Title;
             var year = data.Year;
             var plot = data.Plot;
@@ -135,7 +156,7 @@ else if (userCommand == "movie-this") {
             var language = data.Language;
             var country = data.Country;
             var ratings = data.Ratings
-            console.log("You didn't enter a movie! So we'll give you information on Mr. Nobody.\n\n"+"Movie Info" + "\n" + "____________________" + "\n\n" + "The Title of this Film is: \n" + title + "\n\n" + "The Film was released in the year: \n" + year + "\n\n" + "The Film was rated as follows:\n")
+            console.log("You didn't enter a movie! So we'll give you information on Mr. Nobody.\n\n" + "Movie Info" + "\n" + "____________________" + "\n\n" + "The Title of this Film is: \n" + title + "\n\n" + "The Film was released in the year: \n" + year + "\n\n" + "The Film was rated as follows:\n")
             for (i = 0; i < ratings.length; i++) {
                 console.log("   " + ratings[i].Source + " rated it " + ratings[i].Value + "\n")
             }
@@ -145,7 +166,10 @@ else if (userCommand == "movie-this") {
     }
 }
 else if (userCommand == "do-what-it-says") {
-
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) { console.log(err) }
+        else { console.log(data) }
+    })
 }
 else {
     console.log("User command not recognized")
