527771d7c45bab9b2904b9deb454bd1e6e174b6e
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-08-25 20:28:27 -0400

    added do-what-it-says functionality. put all actions into functions

--- liri.js
+++ liri.js
@@ -16,35 +16,28 @@ var userCommand = process.argv[2]
 var searchTerm = process.argv.splice(3).join("+")
 //adding axios
 var axios = require("axios")
-//log command for testing
-if (userCommand == "log") {
-    console.log(searchTerm)
-}
-//looking up concerts from bandsintown api
-else if (userCommand == "concert-this") {
-    //axios pull from bandsintown api with user's searchTerm
-    axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
-        .then(function (response) {
-            //cleaner way to pull JSON response
+//concert function:
+function concertThis(band) {
+    //do an axios pull for the bandsintown api
+    axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp")
+        .then(function (response) {//get json response
+            //make a var for just the data portion of the response
             var data = response.data
             //if the array has information in it, do the following:
             if (data[0]) {
-                console.log("Your band has the following upcoming events:")
+                console.log("\nYour band has the following upcoming events:\n")
                 for (i = 0; i < data.length; i++) {
-                    console.log((i + 1) + ". " + data[i].lineup + " is playing at \n" + data[i].venue.name + "\n" + "in " + data[i].venue.city + ", " + data[i].venue.region + "\n" + "on " + moment(data[i].datetime).format('LLLL') + "\n")
+                    console.log((i + 1) + ".\n" + data[i].lineup + " is playing at \n" + data[i].venue.name + "\n" + "in " + data[i].venue.city + ", " + data[i].venue.region + "\n" + "on " + moment(data[i].datetime).format('LLLL') + "\n\n")
                 }
             } else { console.log("Your band unfortunately has no upcoming events! :(") }
 
 
         })
-        .catch(function (error) { console.log(error) });
-
 }
-else if (userCommand == "spotify-this-song") {
-    //initial spotify search
+function spotifySong(song) {
     spotify.search({
         type: 'track',
-        query: searchTerm
+        query: song
     },
         function (err, data) {
 
@@ -60,32 +53,7 @@ else if (userCommand == "spotify-this-song") {
                     //console log Ace of Base message
                     console.log("Unfortunately your song isn't listed! So we gave you Ace of Bass" + "\n" + "____________________" + "\n\n")
                     //then do a new search for ace of bass
-                    spotify.search({
-                        type: 'track',
-                        query: 'the sign ace of base'
-                    },
-
-                        function (err, data) {
-                            //if error:
-                            if (err) {
-                                return console.log('Error occured: ' + err);
-                            }
-                            //if new data, play ace of bass
-                            if (data) {
-
-                                var items = data.tracks.items
-
-                                var songName = items[0].name
-                                var artist = items[0].artists[0].name
-                                var album = items[0].album.name
-                                var previewLink = items[0].preview_url
-
-
-                                console.log("The Artist of this Song is: " + artist + "\n\n" + "The Song's name is: " + songName + "\n\n" + "The album that the song is from is: " + album + "\n\n" + "Here is a preview link of the song: \n" + previewLink + "\n")
-                            }
-                        })
-
-
+                    spotifySong("ace of base the sign")
                 }
                 //if there are results
                 else if (data.tracks.total > 0) {
@@ -96,26 +64,22 @@ else if (userCommand == "spotify-this-song") {
                     var artist = items[0].artists[0].name
                     var album = items[0].album.name
                     var previewLink = items[0].preview_url
-                    console.log(artist)
                     console.log("Song Info" + "\n" + "____________________" + "\n\n" + "The Artist of this Song is: " + artist + "\n\n" + "The Song's name is: " + songName + "\n\n" + "The album that the song is from is: " + album + "\n\n" + "Here is a preview link of the song: \n" + previewLink + "\n")
 
                 }
             }
         });
 }
-//if user command is movie-this:
-else if (userCommand == "movie-this") {
-    //if the user inputs a searchterm:
-    if (searchTerm) {
+function movie(movie) {
+    if (movie) {
         //do an axios pull
         axios({
             method: 'get',
-            url: 'http://www.omdbapi.com/?apikey=trilogy&t=' + searchTerm
+            url: 'http://www.omdbapi.com/?apikey=trilogy&t=' + movie
         }).then(function (response) {
             // if it works, display the following:
             var data = response.data;
             if (!data.Error) {
-                console.log(data)
                 //movie title
                 var title = data.Title;
                 //movie release year
@@ -141,34 +105,49 @@ else if (userCommand == "movie-this") {
             else { console.log("I'm sorry but your search returned nothing!") }
         })
     }
-    //if user has no input at all, then
-    else {
-        axios({
-            method: 'get',
-            //display mr nobody
-            url: 'http://www.omdbapi.com/?apikey=trilogy&t=Mr+Nobody'
-        }).then(function (response) {
-            var data = response.data;
-            var title = data.Title;
-            var year = data.Year;
-            var plot = data.Plot;
-            var actors = data.Actors;
-            var language = data.Language;
-            var country = data.Country;
-            var ratings = data.Ratings
-            console.log("You didn't enter a movie! So we'll give you information on Mr. Nobody.\n\n" + "Movie Info" + "\n" + "____________________" + "\n\n" + "The Title of this Film is: \n" + title + "\n\n" + "The Film was released in the year: \n" + year + "\n\n" + "The Film was rated as follows:\n")
-            for (i = 0; i < ratings.length; i++) {
-                console.log("   " + ratings[i].Source + " rated it " + ratings[i].Value + "\n")
-            }
-            console.log("The Film was produced in: \n" + country + "\n\n" + "The Film is available in the following languages: \n" + language + "\n\n" + "The Film had the following actors: \n" + actors + "\n\n" + "Plot summary: \n" + plot)
+}
+//log command for testing
+if (userCommand == "log") {
+    console.log(searchTerm)
+}
 
-        })
+//looking up concerts from bandsintown api
+else if (userCommand == "concert-this") {
+    concertThis(searchTerm);
+}
+//look up songs in spotify
+else if (userCommand == "spotify-this-song") {
+    spotifySong(searchTerm)
+}
+//if user command is movie-this:
+else if (userCommand == "movie-this") {
+    if (searchTerm) {
+        movie(searchTerm)
+    }
+    else {
+        movie("mr nobody")
     }
 }
 else if (userCommand == "do-what-it-says") {
+    //read the file with the commands embedded
     fs.readFile('random.txt', 'utf8', function (err, data) {
+        //if there's an error console log the error
         if (err) { console.log(err) }
-        else { console.log(data) }
+        //otherwise:
+        else {
+            //split the text
+            var nodeData = data.split(",");
+            //and run the following:
+            if (nodeData[0] == "concert-this") {
+                concertThis(nodeData[1])
+            }
+            else if (nodeData[0] == "spotify-this-song") {
+                spotifySong(nodeData[1])
+            }
+            else if (nodeData[0] == "movie-this") {
+                movie(nodeData[1])
+            }
+        }
     })
 }
 else {
